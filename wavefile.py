c8f8fbf569f58e55ada02ddee2ca97886967e121
Takuya Yamamoto <59943729+oozzZZZZ@users.noreply.github.com>
2020-02-02 20:29:22 +0900

    Update wavefile.py

--- wavefile.py
+++ wavefile.py
@@ -10,9 +10,29 @@ def read(filename):
         fs = w.getframerate()
         nframes = w.getnframes()
         frames = w.readframes(nframes)
+
+    #binary -> float    
+    if samplewidth == 2:#16bit
+        data = np.frombuffer(frames, dtype='int16')    
+    elif samplewidth == 3:#24bit
+        a8 = np.fromstring(frames, dtype=np.uint8)
+        tmp = np.empty((nframes, 1, 4), dtype = np.uint8)
+        tmp[:, :, :samplewidth] = a8.reshape(-1, 1, samplewidth)
+        tmp[:, :, samplewidth:] = (tmp[:, :, samplewidth-1:samplewidth] >> 7) * 255
+        data = tmp.view('int32')[:,0,0]       
+    elif samplewidth == 4:#32bit
+        data = np.frombuffer(frames, dtype='int32')
         
-    print(ch,samplewidth,fs,nframes)    
-    
+    return data,fs
+
+def sepalate_read(filename):
+    #data -> binary data
+    with wave.open(filename,mode='rb') as w:
+        ch = w.getnchannels()
+        samplewidth = w.getsampwidth()
+        fs = w.getframerate()
+        nframes = w.getnframes()
+        frames = w.readframes(nframes)   
     #binary -> float    
     if samplewidth == 2:#16bit
         data = np.frombuffer(frames, dtype='int16')
